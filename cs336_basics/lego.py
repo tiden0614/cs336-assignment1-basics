59eefeed0dc8a3981ad705d0321e4b94925054c9
Daryl Z <tiden111@gmail.com>
2025-12-20 14:16:44 -0800

    rmsnorm

--- cs336_basics/lego.py
+++ cs336_basics/lego.py
@@ -1,7 +1,9 @@
 import torch
+from torch import Tensor
 import torch.nn as nn
 import torch.nn.functional as F
 import einx
+from jaxtyping import Float, Int
 
 
 class LinearModule(nn.Module):
@@ -20,7 +22,7 @@ class LinearModule(nn.Module):
         )
         self.W = nn.parameter.Parameter(W)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: Tensor) -> Tensor:
         output = einx.dot("... in, out in -> ... out", x, self.W)
         return output
 
@@ -34,9 +36,38 @@ class EmbeddingModule(nn.Module):
         dtype: torch.dtype = None,
     ):
         super().__init__()
-        embeddings = torch.empty(num_embeddings, embedding_dim, device=device, dtype=dtype)
-        nn.init.trunc_normal_(embeddings, mean=0.0, std=1, a = -3, b = 3)
+        embeddings = torch.empty(
+            num_embeddings, embedding_dim, device=device, dtype=dtype
+        )
+        nn.init.trunc_normal_(embeddings, mean=0.0, std=1, a=-3, b=3)
         self.embeddings = nn.parameter.Parameter(embeddings)
 
-    def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+    def forward(self, token_ids: Int[Tensor, "..."]) -> Tensor:
         return self.embeddings[token_ids]
+
+
+class RMSNormModule(nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-5, device=None, dtype=None):
+        super().__init__()
+        self.d_model = d_model
+        self.eps = eps
+        g = torch.empty(self.d_model, device=device, dtype=dtype)
+        nn.init.trunc_normal_(g, mean=0.0, std=1, a=-3, b=3)
+        self.g = nn.parameter.Parameter(g)
+
+    def forward(self, x: Float[Tensor, "... d_model"]) -> Float[Tensor, "... d_model"]:
+        in_dtype = x.dtype
+        x_f32 = x.to(torch.float32)
+
+        # $$ rms = \sqrt{\frac{1}{d_{model}}\sum_{i=1}^{d_{model}}a_i^2 + \epsilon} $$
+
+        rms = (
+            einx.dot("... d_model, ... d_model -> ... 1", x_f32, x_f32) / self.d_model
+            + self.eps
+        ) ** 0.5
+
+        # $$ RMSNorm(a_i) = \frac{a_i}{RMS(a)}g_i $$
+
+        result = einx.divide("... d_model, ... 1 -> ... d_model", x_f32, rms)
+        result = einx.multiply("... d_model, d_model -> ... d_model", result, self.g)
+        return result.to(in_dtype)
--- tests/adapters.py
+++ tests/adapters.py
@@ -385,7 +385,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    rmsnorm_module = lego.RMSNormModule(d_model, eps)
+    rmsnorm_module.load_state_dict({"g": weights})
+    return rmsnorm_module.forward(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
