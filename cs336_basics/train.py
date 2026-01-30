1584b6129be5201f1ad7b02b8724bd68ef5bd0d4
Daryl Z <tiden111@gmail.com>
2026-01-30 08:21:26 -0800

    checkpointing

--- cs336_basics/train.py
+++ cs336_basics/train.py
@@ -3,6 +3,8 @@ from torch import Tensor
 import numpy as np
 from numpy import typing as npt
 import random
+import os
+import typing
 
 
 def get_batch(
@@ -23,3 +25,28 @@ def get_batch(
     targets_tensor = torch.from_numpy(targets)
 
     return (sequences_tensor.to(device=device), targets_tensor.to(device=device))
+
+
+def save_checkpoint(
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    iteration: int,
+    out: str | os.PathLike | typing.BinaryIO | typing.IO[bytes],
+):
+    obj = {
+        "model": model.state_dict(),
+        "optim": optimizer.state_dict(),
+        "iter": iteration,
+    }
+    torch.save(obj, f=out)
+
+
+def load_checkpoint(
+    src: str | os.PathLike | typing.BinaryIO | typing.IO[bytes],
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+) -> int:
+    obj = torch.load(f=src)
+    model.load_state_dict(obj["model"])
+    optimizer.load_state_dict(obj["optim"])
+    return obj["iter"]
